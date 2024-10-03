d52e46918bfb66b8d752bcf9fd8d1e0edd0cb188
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2024-10-02 21:36:42 -0300

    theme dark and light

--- src/app.tsx
+++ src/app.tsx
@@ -2,15 +2,18 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <>
       <HelmetProvider>
-        <Helmet title="%s | Pizza Shop" />
-        <Toaster richColors duration={2000} />
-        <RouterProvider router={router} />
+        <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+          <Helmet title="%s | Pizza Shop" />
+          <Toaster richColors duration={2000} />
+          <RouterProvider router={router} />
+        </ThemeProvider>
       </HelmetProvider>
     </>
   )
