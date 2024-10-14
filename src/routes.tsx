f829389598b63d66c1ea83b73b8435eaad4e071a
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2024-10-14 11:48:18 -0300

    finalizando frontend

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,15 +2,17 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './page/_layout/app'
 import { AuthLayout } from './page/_layout/auth'
-import { Dashboard } from './page/app/dashboard'
+import { NotFound } from './page/404'
 import { Orders } from './page/app/orders/orders'
 import { SignIn } from './page/auth/sign-in'
 import { SignUp } from './page/auth/sign-up'
+import { Dashboard } from './page/dashboard/dashboard'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
