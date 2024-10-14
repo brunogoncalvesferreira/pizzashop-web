68ea6cef05b69cb069ef0b6601ce540d6e333475
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2024-10-14 12:02:59 -0300

    config react query

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,10 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
@@ -10,9 +12,11 @@ export function App() {
     <>
       <HelmetProvider>
         <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
-          <Helmet title="%s | Pizza Shop" />
+          <Helmet title="%s | Pizza.Shop" />
           <Toaster richColors duration={2000} />
-          <RouterProvider router={router} />
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
         </ThemeProvider>
       </HelmetProvider>
     </>
